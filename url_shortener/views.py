f3dc33a00f014cfbfcbd117525f9d17838202b04
Bhaskar <saibhaskar2995@gmail.com>
2023-04-18 22:48:25 +0530

    Adding documentation to the code

--- url_shortener/views.py
+++ url_shortener/views.py
@@ -8,10 +8,19 @@ from .models import Url
 import string
 import random
 
-
-
 class Home(View):
+    """
+    This is a class based view which has four methods: get, post, delete, and put.
+    """
+     
     def get(self, request, *args, **kwargs):
+        """
+        This method handles GET requests to the given url.
+        --> If the URL contains a short URL, it redirects the user to the corresponding long URL. 
+        --> If the short URL does not exist, it returns a JSON response with an error message.
+        --> If there is no short URL in the URL, it returns a JSON response with a welcome message.
+        """
+
         if 'url' in kwargs:
             try:
                 url = Url.objects.get(short_url = kwargs['url'])
@@ -19,9 +28,15 @@ class Home(View):
             except:
                 message = "Short Url Doesnot match. Please create short url using POST method"
                 return JsonResponse(message,safe=False)
-        return JsonResponse('Welcome to url shortener', safe= False)
+        return JsonResponse('Welcome to URL shortener website', safe= False)
     
     def post(self, request, *args, **kwargs):
+        """
+        This method handles POST requests to the given url.
+        --> It expects a JSON payload with a "long_url" key that contains the long URL to be shortened. It saves the short_url into the database
+        --> If a short URL already exists for the given long URL, it returns a JSON response with the existing short URL.
+        """
+         
         data = json.loads(request.body)
         long_url  = data.get('long_url')
         N = 7
@@ -37,6 +52,12 @@ class Home(View):
         return JsonResponse(data)
     
     def delete(self, request, *args, **kwargs):
+        """
+        This method handles DELETE requests for the given link.
+        --> If the URL contains a short URL, it deletes the corresponding URL from the database and returns a JSON response with a success message. 
+        --> If the short URL does not exist, it returns a JSON response with an error message.
+        """
+
         if 'url' in kwargs:
             try:
                 url = Url.objects.get(short_url = kwargs['url'])
@@ -48,6 +69,13 @@ class Home(View):
                 return JsonResponse(message,safe=False)
             
     def put(self,request, *args, **kwargs):
+        """
+        This method handles PUT requests for the given link.
+        --> If the URL contains a short URL, it expects a JSON payload with a "long_url" key that contains the new long URL. 
+        --> It updates the corresponding URL in the database and returns a JSON response with a success message. 
+        --> If the short URL does not exist, it returns a JSON response with an error message.
+        """
+
         if 'url' in kwargs:
             try:
                 data = json.loads(request.body)
